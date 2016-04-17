deb56716b8d483f2a332bd7f7c6178d48950832e
ProfNimnul <stanso@ukr.net>
2016-04-17 23:01:00 +0300

    Разобраться, как хранить и группировать данные

--- Birthday.py
+++ Birthday.py
@@ -1,6 +1,12 @@
 import xlrd
 import xlwt
-from easygui import fileopenbox
+import datetime
+from easygui import fileopenbox,msgbox
+
+
+def update_person(info, birthday=list()):
+    birthday.add(info)
+    return birthday
 
 
 fname = fileopenbox("Выберите файл с днями рождения", "")
@@ -8,12 +14,30 @@ if not fname:
     exit()
 if not fname.endswith(".xlsx"):
         msgbox("Выбран не файл xlsx", ok_button="Закрыть", title="Проверьте тип файла!")
-
+        exit()
 rb = xlrd.open_workbook(fname)
+all_persons = list()
+persons_with_one_birth_data=dict()
 
 sheets_list = rb.sheet_names() #нашли количество листов в книге
-ws=rb.sheet_by_index(len(sheets_list)-1)
+# ws=rb.sheet_by_index(len(sheets_list)-1)
+sheet=rb.sheet_by_index(len(sheets_list)-1)
 # print(dirname)
-vals = [ws.row_values(rownum) for rownum in range(ws.nrows)]
+for rownum in range(sheet.nrows):
+
+    rows=sheet.row_values(rownum)
+    if sheet.cell(rownum,1).ctype !=3:
+        continue
+    date_tuple = xlrd.xldate_as_tuple(rows[1],rb.datemode)
+    full_date =  datetime.datetime(*date_tuple)
+
+    day_of_birth=date_tuple[2] #єто день рождения (именно ДЕНЬ)
+    info = sheet.cell(rownum, 0) + "$" + sheet.cell(rownum, 2)  # сформировали строку из ФИО и адреса
+
+    if  not all_persons.get(day_of_birth,None): #такой даты нет вообще
+        persons_with_one_birth_data.update(str(day_of_birth),update_person(info,all_persons))
+    else
+        persons_with_one_birth_data[str(day_of_birth)]=update_person()
+
 pass
 exit()
